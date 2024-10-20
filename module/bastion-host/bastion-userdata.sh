562da33bd358b5401f7e60298a5e2bb38e19b64c
nicmboso <devops.wizzy@gmail.com>
2024-10-20 18:20:13 +0100

    creating BastionHost, Nexus and Sonarqube instances

---
+++ module/bastion-host/bastion-userdata.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "${var.private-key}" >> /home/ec2-user/.ssh/id_rsa
+chmod 400 /home/ec2-user/.ssh/id_rsa
+sudo chown ec2-user:ec2-user /home/ec2-user/.ssh/id_rsa
+# sudo yum install mysql-server -y
+sudo hostnamectl set-hostname bastion
